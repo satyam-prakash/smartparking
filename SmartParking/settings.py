c68fa2029f06ab5bbd6866b75dedf8238488b4eb
Satyam Prakash <prakashsatyam29@gmail.com>
2025-12-24 11:13:01 +0530

    Disable HTTPS redirect for HTTP access

--- SmartParking/settings.py
+++ SmartParking/settings.py
@@ -137,9 +137,10 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Security settings for production
 if not DEBUG:
-    SECURE_SSL_REDIRECT = True
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
+    # Temporarily disabled SSL redirect until certificate is configured
+    # SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = False  # Set to True when HTTPS is enabled
+    CSRF_COOKIE_SECURE = False  # Set to True when HTTPS is enabled
     SECURE_BROWSER_XSS_FILTER = True
     SECURE_CONTENT_TYPE_NOSNIFF = True
     X_FRAME_OPTIONS = 'DENY'
